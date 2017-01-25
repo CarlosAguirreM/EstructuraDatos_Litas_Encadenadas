e6c7166ee389a64cbd8631e2c27f4fdc33ca4080
CarlosAguirreM <darksorlac1197@gmail.com>
2017-01-24 19:11:13 -0500

    metodos recorrer en ambas direcciones, buscar y eliminar por posicion y por contenido

--- src/ListasCircularesEncadenadasDobles/Aplicacion.java
+++ src/ListasCircularesEncadenadasDobles/Aplicacion.java
@@ -11,25 +11,51 @@ public class Aplicacion {
 	        System.out.println("************* Ejemplo de lista Doble ************\n");
 	        
 	        lista.insertarInicio(new NodoDC("1"));
-	        lista.insertarFinal(new NodoDC("2"));
-	        /*lista.agregarNodoInicio(new NodoD("3", null, null));
-	        lista.agregarNodoInicio(new NodoD("4", null, null));
-	        lista.agregarNodoInicio(new NodoD("5", null, null));
-	        lista.agregarNodoInicio(new NodoD("6", null, null));
-	        lista.agregarNodoInicio(new NodoD("7", null, null));
-	        lista.agregarNodoInicio(new NodoD("8", null, null));
-	        lista.agregarNodoInicio(new NodoD("9", null, null));
-	        lista.agregarNodoInicio(new NodoD("10", null, null));
-	        
-	        System.out.println("\n\n***** Lista de inicio a fin*****");
-	        System.out.println("\n"+lista.recorrerListaAscendente());
-	        
-	        System.out.println("\n\n***** Lista de fin a inicio*****");
-	        System.out.println(lista.recorrerListaDescendente());
+	        lista.insertarInicio(new NodoDC("2"));
+	        lista.insertarInicio(new NodoDC("3"));
+	        lista.insertarInicio(new NodoDC("4"));
+	        lista.insertarInicio(new NodoDC("5"));
+	        lista.insertarInicio(new NodoDC("6"));
+	        lista.insertarInicio(new NodoDC("7"));
+	        lista.insertarInicio(new NodoDC("8"));
+	        lista.insertarFinal(new NodoDC("-1"));
+	        lista.insertarFinal(new NodoDC("-2"));
+	        lista.insertarFinal(new NodoDC("-3"));
+	        lista.insertarFinal(new NodoDC("-4"));
+	        lista.insertarFinal(new NodoDC("-5"));
 	        
 	        System.out.println("\n\n***** Tamaño ******");
 	        System.out.println(lista.getTamaño());
-	        	        	        
+	        
+	        System.out.println("\n\n***** Consulta si existe el elemento 2 ******");
+	        System.out.println(lista.buscarContenido("2").getElemento());
+	        
+	        System.out.println("\n\n***** Obtener poicion del elemento 2 ******");
+	        System.out.println(String.valueOf(lista.getPosicion("2")));
+	        
+	        System.out.println("\n\n***** Consulta si existe el nodo de posicion 1 ******");
+	        System.out.println(lista.buscarNodoPosicion(1).getElemento());
+	        
+	        System.out.println("\n\n***** Tamaño ******");
+	        System.out.println(lista.getTamaño());
+	        
+	        System.out.println("\n\n***** Imprimir lista ******");
+	        System.out.println(lista.recorrerSig());
+	        
+	        System.out.println("\n\n***** Imprimir lista inverso******");
+	        System.out.println(lista.recorrerAnt());
+	        
+	        System.out.println("\n\n***** Eliminar Posicion ******");
+	        lista.eliminarNodoPosición(0);
+	        System.out.println(lista.recorrerSig());
+	        
+	        System.out.println("\n\n***** Eliminar Contenido ******");
+	        lista.eliminarNodoContenido("-3");
+	        System.out.println(lista.recorrerSig());
+	        
+	        /*
+	        
+	             
 	        System.out.println("\n\n********Ingresa un nodo nuevo al inicio**********");
 	        lista.agregarNodoInicio(new NodoD("45", null, null));
 	        System.out.println("\n"+lista.recorrerListaAscendente());
--- src/ListasCircularesEncadenadasDobles/ListaCircular.java
+++ src/ListasCircularesEncadenadasDobles/ListaCircular.java
@@ -1,17 +1,17 @@
 package ListasCircularesEncadenadasDobles;
 
 public class ListaCircular {
-	
+
 	protected NodoDC cabeza;
 	protected int tamaño;
-	
+
 	public ListaCircular(){
-		
+
 		tamaño=0;
 		cabeza=null;
-				
+
 	}
-	
+
 	public int getTamaño(){
 		return tamaño;
 	}
@@ -20,66 +20,240 @@ public class ListaCircular {
 
 		return getTamaño()==0;
 	}
-	
+
 	public void insertarInicio(NodoDC nuevoNodo){
-		
+
 		if(verificarVacia()==true){
-			
+
 			cabeza=nuevoNodo;
-			
+			cabeza.setSiguiente(cabeza);
+			cabeza.setAnterior(cabeza);
+
 		}else{
-			
+
 			NodoDC auxNodo = cabeza;
-			
+
 			while(auxNodo.getSiguiente()!=cabeza){
-				
+
 				auxNodo=auxNodo.getSiguiente();
 			}
-			
+
 			auxNodo.setSiguiente(nuevoNodo);
 			nuevoNodo.setAnterior(auxNodo);
 			nuevoNodo.setSiguiente(cabeza);
 			cabeza.setAnterior(nuevoNodo);
 			cabeza=nuevoNodo;
-			
+
 		}
-		
-		
+
+		tamaño++;
 	}
-	
+
 	public void insertarFinal(NodoDC nuevoNodo){
-		
+
 		if(verificarVacia()==true){
-			
+
 			cabeza=nuevoNodo;
-			
+			cabeza.setSiguiente(cabeza);
+			cabeza.setAnterior(cabeza);
+
 		}else{
-			
+
 			NodoDC auxNodo = cabeza;
-			
+
 			while(auxNodo.getSiguiente()!=cabeza){
-				
+
 				auxNodo=auxNodo.getSiguiente();
 			}
-			
+
 			auxNodo.setSiguiente(nuevoNodo);
 			nuevoNodo.setAnterior(auxNodo);
 			nuevoNodo.setSiguiente(cabeza);
 			cabeza.setAnterior(nuevoNodo);
-						
+
 		}
-		
-		
+		tamaño++;
+
 	}
-	
-	public void insertarOrden(NodoDC nuevoNodo){
-		
+
+	public NodoDC buscarContenido(String elemento){
+
+		NodoDC auxNodo=cabeza;
+		while((auxNodo.getSiguiente()!=cabeza)&&!(auxNodo.getElemento().equals(elemento))){
+
+			auxNodo=auxNodo.getSiguiente();
+
+		}
+		return auxNodo;
+	}
+
+	public NodoDC buscarNodoPosicion(int posicion){
+
+		NodoDC nodoAux=cabeza; 
+		NodoDC nodoEncontrado=null;
+		int i=0;
+
+		if( posicion>tamaño || posicion <0){
+
+			nodoEncontrado=null;
+
+		}else if(posicion==0){
+
+			nodoEncontrado=cabeza;
+
+		}else{
+
+			while(nodoAux.getSiguiente()!=cabeza){
+
+				nodoAux=nodoAux.getSiguiente();
+				i++;
+
+				if(i==posicion)
+					nodoEncontrado=nodoAux;
+
+			}
+		}
+
+		return nodoEncontrado;
+	}
+
+	public int getPosicion(String elemento){
+
+		NodoDC auxNodo=cabeza;
+		int indice=0;
+
+		while((auxNodo.getSiguiente()!=cabeza)&&!(auxNodo.getElemento().equals(elemento))){
+
+			auxNodo=auxNodo.getSiguiente();
+			indice++;
+
+		}
+		return indice;
+	}
+
+	public String recorrerSig(){
+
+		String lista="";
+
 		if(verificarVacia()==true){
-			
-			cabeza=nuevoNodo;
-			
+
+			lista="Está vacía";
+
+		}else{
+
+			NodoDC nodoAux=cabeza;
+			int i=0;
+
+			//System.out.print("Inicio <--> ");
+
+			do{
+
+				lista += (i + ". [ "+ nodoAux.getElemento()+" ] <-> ");
+				nodoAux=nodoAux.getSiguiente();
+
+				i++;
+
+			}while(nodoAux.getSiguiente()!=cabeza);
+
+			lista += (i + ". [ "+ nodoAux.getElemento()+" ] <-> ");
+			lista += ("NULL");
+
+		}
+
+		return lista;
+
+	}
+
+	public String recorrerAnt(){
+
+		String lista="";
+
+		if(verificarVacia()==true){
+
+			lista="Está vacía";
+
+		}else{
+
+			NodoDC nodoAux=cabeza;
+			int i=0;
+
+			//System.out.print("Inicio <--> ");
+
+			do{
+
+				lista += (i + ". [ "+ nodoAux.getElemento()+" ] <-> ");
+				nodoAux=nodoAux.getAnterior();
+
+				i++;
+
+			}while(nodoAux.getAnterior()!=cabeza);
+
+			lista += (i + ". [ "+ nodoAux.getElemento()+" ] <-> ");
+			lista += ("NULL");
+
+		}
+
+		return lista;
+
+	}
+
+	public void eliminarNodoPosición(int posicion){
+
+		NodoDC nodoAux = null;
+		NodoDC nodoAuxAnt=null;
+		NodoDC nodoAuxSig=null;
+
+		nodoAux=buscarNodoPosicion(posicion);
+
+		if(nodoAux!=null){
+
+			if(posicion==0){
+
+				nodoAuxAnt=nodoAux.getAnterior();
+				nodoAuxAnt.setSiguiente(nodoAux.getSiguiente());
+				nodoAux=nodoAux.getSiguiente();
+				nodoAux.setAnterior(nodoAuxAnt);
+				cabeza=nodoAux;
+
+			}else{
+
+
+				nodoAuxAnt=nodoAux.getAnterior();
+				nodoAuxSig=nodoAux.getSiguiente();
+
+				nodoAuxAnt.setSiguiente(nodoAuxSig);
+				nodoAuxSig.setAnterior(nodoAuxAnt);
+
+				nodoAux.setSiguiente(null);
+				nodoAux.setAnterior(null);
+			}
+
 		}
 	}
 
+	public void eliminarNodoContenido(String elemento){
+
+		NodoDC nodoAux = null;
+		NodoDC nodoAuxAnt=null;
+		NodoDC nodoAuxSig=null;
+
+		nodoAux=buscarContenido(elemento);
+
+		if(nodoAux!=null){
+
+
+			nodoAuxAnt=nodoAux.getAnterior();
+			nodoAuxSig=nodoAux.getSiguiente();
+
+			nodoAuxAnt.setSiguiente(nodoAuxSig);
+			nodoAuxSig.setAnterior(nodoAuxAnt);
+
+			nodoAux.setSiguiente(null);
+			nodoAux.setAnterior(null);
+
+
+		}
+
+	}
 
 }
