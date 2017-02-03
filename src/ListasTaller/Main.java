6a58b14a9369bbfad612c08a03d7c2cd64cb2dde
CarlosAguirreM <darksorlac1197@gmail.com>
2017-02-02 20:45:58 -0500

    agregar taller parte 2, lista de productos

--- src/ListasTaller/Main.java
+++ src/ListasTaller/Main.java
@@ -2,7 +2,7 @@ package ListasTaller;
 
 import ListasEncadenadasSimples.Lista;
 
-public class Main {
+/*public class Main {
 	
 	 static ListaAlumnos lista = new ListaAlumnos();
 	
@@ -63,5 +63,62 @@ public class Main {
 		}
 		
 	}
+
+}*/
+
+
+public class Main {
+
+	public static void main(String[] args) {
+		SLinkedList lista_productos= new SLinkedList();
+		
+		System.out.println("----Cargando Inventario----");
+		System.out.println("cargando atun...");
+		lista_productos.addFirst(NuevoNodo("atun", 3, "no listo"));
+		
+		System.out.println("cargando panes...");
+		lista_productos.addFirst(NuevoNodo("pan", 4, "no listo"));
+		
+		System.out.println("cargando galletas...");
+		lista_productos.addFirst(NuevoNodo("galleta", 2, "no listo"));
+		
+		System.out.println("cargando limones...");
+		lista_productos.addFirst(NuevoNodo("limon", 2, "no listo"));
+		System.out.println(lista_productos.imprimirLista());
+	
+	
+	System.out.println("----Ingresando lista de cliente----");
+	System.out.println("comprando atun (2)...");
+	lista_productos.editarNodoContenido("atun",2,"listo");
+	
+	
+	System.out.println("comprando pan (1)...");
+	lista_productos.editarNodoContenido("pan",1,"listo");
+	
+	
+	System.out.println("comprando galletas (2)...");
+	lista_productos.editarNodoContenido("galleta",2,"listo");
+	
+
+	System.out.println("comprando limones (1)...");
+	lista_productos.editarNodoContenido("limon",1,"listo");
+	
+	
+	System.out.println("----Actualizando Inventario----");
+	System.out.println(lista_productos.imprimirLista());
+	
+	
+	
+	}
+	
+	public static Nodo NuevoNodo(String el,int n,String e){
+		Nodo N =null;
+			N = new Nodo(el,e,n,null);				
+		return N;
+	}
+	
+	
+	
+	
 
 }
---
+++ src/ListasTaller/Nodo.java
@@ -0,0 +1,44 @@
+package ListasTaller;
+
+public class Nodo {
+	
+	private String elemento;	//Nombre del producto
+	private String estado;		//Esta listo o no
+	private int 	canti=0;
+	private Nodo next;			// siguiente nodo
+	public Nodo(String el,String e,int c,Nodo n){
+		elemento 	= el;
+		estado		= e;
+		canti		= c;
+		next		= n;
+		
+	}
+	public Nodo(String el,String e,int c){
+		elemento	= el;
+		estado		= e;
+		canti		= c;
+		next		= null;
+	}
+	public Nodo(String el){
+		elemento = el;
+		estado   =	"nolisto";
+		next	 =	null;
+	}
+	
+	
+	public String getElemento(){return elemento;}
+	
+	public Nodo getnext(){return next;}
+	
+	public void setElement(String newElem){elemento = newElem;}
+	
+	public void setNext(Nodo newNext){next = newNext;}
+	
+	public String getEstado() {return estado;}
+	
+	public void setEstado(String newestado) {estado = newestado;}
+	
+	public int getCantidad() {return canti;}
+	
+	public void setCantidad(int newc) {canti = newc;}
+}
